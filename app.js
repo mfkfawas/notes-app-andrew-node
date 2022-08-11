49c20a78653afd4d5368b8c8118d4a883056e97b
mfkfawas <mfkfawas@gmail.com>
2022-08-11 15:01:09 +0530

    chalk package

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 // In the package.json file add "type" : "module", Adding this enables ES6 modules
-import validator from 'validator';
+// import validator from 'validator';
+import chalk from 'chalk';
 import getNotes from './notes.js';
 
 const msg = getNotes();
-
 console.log(msg);
 // console.log(validator.isEmail('andrew@example.com'));
-console.log(validator.isURL('www.google.com'));
+// console.log(validator.isURL('www.google.com'));
+
+// console.log(chalk.bgBlack.green.bold('Success!'));
+console.log(chalk.green.inverse.bold.underline('Success!'));
